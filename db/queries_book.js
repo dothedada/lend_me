e2f27afa76cbd60e2391693bab06b237cd47cc81
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-04-23 08:14:01 -0500

    feat(db): Adds the books queries

--- db/queries_book.js
+++ db/queries_book.js
@@ -1,6 +1,51 @@
 import newPool from './pool.cjs';
 
+const booksQuery = `
+SELECT 
+	books.title AS title, 
+	authors.name AS author,
+	editorials.name AS editorial,
+	books.year AS year, 
+	books.sinopsys AS sinopsys, 
+	books.image AS photo, 
+	books.url AS url, 
+FROM books 
+JOIN authors ON books.author_id = authors.id 
+JOIN editorials ON books.editorial_id = editorials.id`;
+
 export const getAllBooks_db = async () => {
-    const { rows } = await newPool.query('SELECT * FROM books');
+    const { rows } = await newPool.query(booksQuery);
     return rows;
+};
+
+export const getBooksBy_db = async (parameter, value) => {
+    const validParameters = ['title', 'author', 'editorial', 'year'];
+    if (!validParameters.includes(parameter)) {
+        throw new Error(`Invalid query parameter '${parameter}'`);
+    }
+
+    let query = `${booksQuery} WHERE `;
+    query += parameter === 'year' ? 'year = $1' : `${parameter} ILIKE $1`;
+    const cleanValue = parameter === 'year' ? value : `%${value}%`;
+
+    try {
+        const { rows } = await newPool.query(query, [cleanValue]);
+        return rows;
+    } catch (err) {
+        throw new Error(`Database query failed: ${err}`);
+    }
+};
+
+export const getBooksWith_db = async (value) => {
+    const fields = ['title', 'author', 'editorial', 'sinopsys']
+        .map((field) => `${field} ILIKE $1`)
+        .join(' OR ');
+    const query = `${booksQuery} WHERE ${fields}`;
+
+    try {
+        const { rows } = await newPool.query(query, [`%${value}%`]);
+        return rows;
+    } catch (err) {
+        throw new Error(`Database query failed: ${err}`);
+    }
 };
