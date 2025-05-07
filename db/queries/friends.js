996995298f5ee2f072858b0d3b76e6761aac1716
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-06 20:06:25 -0500

    adds friend requests db and methods

--- db/queries/friends.js
+++ db/queries/friends.js
@@ -109,7 +109,81 @@ const removeUser_db = async (userId) => {
     }
 };
 
+const addRequest_db = async (from, to, message) => {
+    if (!from || !to || !message) {
+        throw new Error('Some paramaters are missing');
+    }
+
+    const query = `
+	INSERT INTO friend_request (from_id, to_id, message)
+	VALUES ($1, $2, $3)
+	RETURNING *`;
+
+    try {
+        const { rows } = pool.query(query, [from, to, message]);
+        return rows;
+    } catch (err) {
+        throw new Error(
+            `Database query to 'friend_reques' failed: ${err.message}`,
+        );
+    }
+};
+
+const cancelReques_db = async (requestId) => {
+    if (!requestId || isNaN(requestId)) {
+        throw new Error(`Must provide a valid id: '${requestId}`);
+    }
+
+    const query = `
+	DELETE FROM friend_request 
+	WHERE id = $1
+	RETURNING *`;
+
+    try {
+        const { rows } = pool.query(query, [requestId]);
+        return rows;
+    } catch (err) {
+        throw new Error(
+            `Database query to 'friend_reques' failed: ${err.message}`,
+        );
+    }
+};
+
+const getFriendRequests_db = async (fromOrTo, id) => {
+    if (!fromOrTo || !id || isNaN(id)) {
+        throw new Error(`Must provide a valid params, id: '${requestId}`);
+    }
+
+    const filter =
+        fromOrTo === 'from' ? 'friend_request.from_id' : 'friend_request.to_id';
+
+    const query = `
+	SELECT
+		friend_request.id AS id,
+		friend_request.from_id AS from_id,
+		users_a.name AS from_name,
+		friend_request.to_id AS to_id,
+		users_b.name AS to_name,
+		friend_request.message AS message
+	FROM friend_request
+	JOIN users users_a ON friend_request.from_id = users_a.id
+	JOIN users users_b ON friend_request.to_id = users_b.id
+	WHERE ${filter} = $1`;
+
+    try {
+        const { rows } = pool.query(query, [id]);
+        return rows;
+    } catch (err) {
+        throw new Error(
+            `Database query to 'friend_reques' failed: ${err.message}`,
+        );
+    }
+};
+
 export const friends_db = {
+    makeRequest: addRequest_db,
+    cancelRequest: cancelReques_db,
+    getRequests: getFriendRequests_db,
     getFriends: getAllFriends_db,
     friendship: checkFriendship_db,
     addFriendship: addFriend_db,
