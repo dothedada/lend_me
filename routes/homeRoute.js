d101a701869f886bb687d11acd2b63fa12b7a2c2
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-11 11:19:11 -0500

    feat(layout): Uverhaul the dashboard, books and search sections

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -7,21 +7,11 @@ const homeRoute = Router();
 homeRoute.get('/', getUserTransactions, getOwnedBooks, async (req, res) => {
     const userData = req.user;
     const { requested, active, denied } = res.transactions;
-    const [borrowed, lended] = active.reduce(
-        (acc, book) => {
-            const insertionInd = book.owner_user_id !== +userData.id ? 0 : 1;
-            acc[insertionInd].push(book);
-            return acc;
-        },
-        [[], []],
-    );
 
     res.render('dashboard.ejs', {
         user: userData,
-        requestsBooks: requested ?? [],
-        deniedRequest: denied ?? [],
-        borrowedBooks: borrowed,
-        lendedBooks: lended,
+        requestsBooks: [...requested, ...denied],
+        activeBooks: active,
         userBooks: res.books.user,
     });
 });
