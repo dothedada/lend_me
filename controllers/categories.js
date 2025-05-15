0a0aa16574700c56ae66453e62b27fe7b336c994
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-15 17:07:07 -0500

    feat(categories): Adds error handling for categories middleware

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,8 +1,14 @@
 import { categories_db } from '../db/queries/simpleQuerys.js';
+import { asyncWrapper } from './middleware.js';
+import { CustomErr, errorMsg } from './validations.js';
 
-export const getAllCategories = async (req, res, next) => {
+export const getAllCategories = asyncWrapper(async (req, res, next) => {
     const categories = await categories_db.get();
+    if (categories.length === 0) {
+        throw new CustomErr(errorMsg.categories.noItems, 404, 'noItemsData');
+    }
+
     res.categories = categories;
 
     next();
-};
+});
