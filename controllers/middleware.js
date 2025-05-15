1349644d258bbaccdc7d48e20f73ebae2bba10ca
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-15 16:18:43 -0500

    feat(chore): adds an error handler wrapper for the async middleware functions

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -1,5 +1,18 @@
 import { validationResult, matchedData } from 'express-validator';
 
+export const asyncWrapper = (asyncCB) => {
+    if (!(asyncCB instanceof (async () => {}).constructor)) {
+        throw new Error(`'${asyncCB}' is not an async function`);
+    }
+
+    return (...params) => {
+        const promisedReturn = asyncCB(...params);
+        const next = params[params.length - 1];
+
+        return Promise.resolve(promisedReturn).catch(next);
+    };
+};
+
 export const setValidationResult = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
