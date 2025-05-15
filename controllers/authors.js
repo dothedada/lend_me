c2ca638a8ba7b2fbe183501c1686b0202910d67d
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-15 16:30:25 -0500

    feat(Authors): adds error handling for the authors async middleware

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,35 +1,48 @@
 import { authors_db } from '../db/queries/simpleQuerys.js';
-import { authorRules } from './validations.js';
-import { setValidationResult } from './middleware.js';
+import { authorRules, CustomErr } from './validations.js';
+import { asyncWrapper, setValidationResult } from './middleware.js';
 
-export const getAllAuthors = async (req, res, next) => {
+export const getAllAuthors = asyncWrapper(async (req, res, next) => {
     const authors = await authors_db.get();
+    if (authors.length === 0) {
+        throw new CustomErr('No authors where found', 404, 'noItemsData');
+    }
+
     res.authors = authors;
 
     next();
-};
+});
 
-export const getAuthorData = async (req, res, next) => {
+export const getAuthorData = asyncWrapper(async (req, res, next) => {
     const { authorId } = req.params;
     if (!authorId) {
-        throw new Error('No author id on the params');
+        throw new CustomErr('No author id on the params', 404, 'noItemsData');
     }
+
     const authorData = await authors_db.get({ id: authorId });
+    if (!authorData) {
+        throw new CustomErr('No author with the given id', 404, 'noItemsData');
+    }
+
     res.author = authorData;
 
     next();
-};
+});
 
 export const authorValidation = [authorRules, setValidationResult];
 
-export const updateAuthorData = async (req, res, next) => {
+export const updateAuthorData = asyncWrapper(async (req, res, next) => {
     if (res.errors !== undefined) {
         return next();
     }
 
     const authorUpdatedData = req.body;
     const updatedData = await authors_db.put(authorUpdatedData);
+    if (!updatedData) {
+        throw new CustomErr("Unknown error while updatin author's data", 500);
+    }
+
     res.author = updatedData;
 
     next();
-};
+});
